a192dd6fae9c20e6061bc9e391996166ec858ab2
kagg-design <info@kagg.eu>
2019-08-11 18:31:54 +0300

    Change namespace to KAGG.

---
+++ tests/phpunit/class-kagg-testcase.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * KAGG_TestCase class file.
+ *
+ * @package KAGG\Disable_Plugins
+ */
+
+namespace KAGG;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class KAGG_TestCase
+ */
+class KAGG_TestCase extends TestCase {
+	/**
+	 * Setup test
+	 */
+	public function setUp() {
+		parent::setUp();
+		\WP_Mock::setUp();
+	}
+
+	/**
+	 * End test
+	 */
+	public function tearDown() {
+		\WP_Mock::tearDown();
+		parent::tearDown();
+	}
+}
